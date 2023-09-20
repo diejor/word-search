f312dd6f9def4607519591ad9b6c558eafc482ca
diejor <diejorarr@gmail.com>
2023-09-20 16:59:01 -0500

    project started

---
+++ HW2-CS2337-DiegoR-R.cpp
@@ -0,0 +1,17 @@
+/*
+    Snake Game
+    Diego R.R.
+    09/20/2023
+    CS2337.501
+    A snake game that runs in the console.
+    Changelog: 
+        09/20/2023 - project started
+    Notes:
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
